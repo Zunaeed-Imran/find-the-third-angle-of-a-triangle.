f72f39aadb0dc9d1d655c30af6f2b352b174c01c
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-04-29 20:47:45 +0600

    Third angle of triangle in javaScript

---
+++ third_triangle.js
@@ -0,0 +1,7 @@
+let a = prompt("Enter the 1st length of triangle: ");
+let b = prompt("Enter the 2nd length of triangle: ");
+a = Number(a);
+b = Number(b);
+let result =180 - (a + b);
+
+document.write(result);
